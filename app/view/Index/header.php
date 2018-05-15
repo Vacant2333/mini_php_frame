02c7a3cc4f0cd21f187430a5b110c2f81f1bc43f
Vacant233 <19872346+Vacant233@users.noreply.github.com>
2018-05-15 19:25:57 +0800

    background

--- app/view/Index/header.php
+++ app/view/Index/header.php
@@ -2,7 +2,16 @@
 <head>
     <title>TEST TITLE</title>
 </head>
-<body>
+<body style="
+background-image: url(/public/img/bg.jpg);
+background-attachment: fixed;
+background-repeat: no-repeat;
+background-position: center center;
+background-size: cover;
+left: 0px;
+top: 0px;
+height: 100%;
+">
 <header>
 HEADER
 </header>
--- app/view/Index/page/index.php
+++ app/view/Index/page/index.php
@@ -1,1 +1,3 @@
 INDEX内容<?php var_dump($this->parameter)?>
+<br>
+文件位于 /app/view/page/index.php
