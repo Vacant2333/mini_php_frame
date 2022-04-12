fbdf769f4311bd18159180aef8f1aac949f9c30f
Vacant2333 <vacant2333@gmail.com>
2022-04-13 00:50:52 +0800

    Changes

--- miniphp/config/Config.php
+++ miniphp/config/Config.php
@@ -1,25 +1,25 @@
 <?php
 //框架全局设置
-define("__CONFIG__", array(
-		//加密Salt
+const __CONFIG__ = array(
+	//加密Salt
 		'crypt_salt' => "yourCryptSalt",
 
-		//数据库配置
+	//数据库配置
 		'db' => array(
 			//地址
-			'ip' => 'localhost',
+				'ip' => 'localhost',
 			//用户名
-			'user' => 'root',
+				'user' => 'root',
 			//用户密码
-			'password' => 'password',
+				'password' => 'password',
 			//连接的库名
-			'name' => 'db_name'
+				'name' => 'db_name'
 		)
-	));
+);
 
 //自定义路由
-define('__ROUTE__', array(
+const __ROUTE__ = array(
 		'/register/' => "/Index/register",
 		'/loginxxx/' => "/Index/login/xxx",
 		'/archive/2/' => "/Archive/list/2",
-	));
+);
